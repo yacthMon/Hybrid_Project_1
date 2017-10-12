783e2c96a74e35670993ec66745faab2e4dc110e
yacth_Mon <yacthmon@gmail.com>
2017-10-13 01:18:13 +0700

    Add tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { TopUpdatePage } from '../pages/top-update/top-update';
+import { AboutMePage } from '../pages/about-me/about-me';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,9 +13,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    TopUpdatePage,
+    AboutMePage,
     TabsPage
   ],
   imports: [
@@ -26,9 +24,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    TopUpdatePage,
+    AboutMePage,
     TabsPage
   ],
   providers: [
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { TopUpdatePage } from '../top-update/top-update';
+import { AboutMePage } from '../about-me/about-me';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  TopUpdate = TopUpdatePage;  
+  AboutMe = AboutMePage;
 
   constructor() {
 
