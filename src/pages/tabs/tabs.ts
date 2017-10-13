7f98d923edbe1ddb3bbb484a138858f0944da6a3
yacth_Mon <yacthmon@gmail.com>
2017-10-14 00:29:04 +0700

    Complete Setting Page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { TopUpdatePage } from '../top-update/top-update';
 import { AboutMePage } from '../about-me/about-me';
-
+import { SettingPage } from '../setting/setting';
 @Component({
   templateUrl: 'tabs.html'
 })
@@ -10,7 +10,7 @@ export class TabsPage {
 
   TopUpdate = TopUpdatePage;  
   AboutMe = AboutMePage;
-
+  setting = SettingPage;
   constructor() {
 
   }
