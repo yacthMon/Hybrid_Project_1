45691b3c4462d1026cd1b242addb943776e9466f
yacth_Mon <yacthmon@gmail.com>
2017-10-14 01:06:06 +0700

    Change tab back

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,15 +2,14 @@ import { Component } from '@angular/core';
 
 import { TopUpdatePage } from '../top-update/top-update';
 import { AboutMePage } from '../about-me/about-me';
-import { SettingPage } from '../setting/setting';
+
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
   TopUpdate = TopUpdatePage;  
-  AboutMe = AboutMePage;
-  setting = SettingPage;
+  AboutMe = AboutMePage;  
   constructor() {
 
   }
