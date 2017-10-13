f6d063356c2f419ddb49ce5999d8b467e2a3e9aa
yacth_Mon <yacthmon@gmail.com>
2017-10-13 23:05:08 +0700

    Generate Setting and Bookmark page

---
+++ src/pages/bookmark/bookmark.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the BookmarkPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-bookmark',
+  templateUrl: 'bookmark.html',
+})
+export class BookmarkPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad BookmarkPage');
+  }
+
+}
